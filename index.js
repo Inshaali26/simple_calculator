4137b12bc15deb33964190a6df52b3c5bc12e30e
EshaTariq236 <Tariqesha15@gmail.com>
2024-03-26 09:03:01 +0500

    Adding a Simple CLI Calculator Project

---
+++ index.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// Printing a Wellcome Message
+console.log("\n\tWellcome To \'InshaAli\' - CLI Simple Calculator\n");
+// Asking Quetions from Users Through inquirer
+let answers = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one Operator to perform Operations",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtractions", "Multiplication", "Division"],
+    },
+]);
+// Conditional Statements If-Else
+if (answers.operator === "Addition") {
+    console.log(answers.firstNumber + answers.secondNumber);
+}
+else if (answers.operator === "Subtractions") {
+    console.log(answers.firstNumber - answers.secondNumber);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.firstNumber * answers.secondNumber);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.firstNumber / answers.secondNumber);
+}
+else {
+    console.log("Invalid Input");
+}
